244f485713ce7155fa761fb38c17f20168bc3b23
Akarshana06 <akarshana027@gmail.com>
2025-01-31 15:33:03 +0530

    Changes are made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';  // Import directly
 import SignupPage from './pages/SignupPage';
+import Home from './pages/Home';
 import "./App.css";
 
 
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path= '/' element={<Home/>}/>
         <Route path="/login" element={<LoginPage />} />
         <Route path = "/signin" element={<SignupPage/>} />
       </Routes>
